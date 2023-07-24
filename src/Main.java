0376a7e34f85cc39ec608fde9e860398c63371bc
Sona <s.harutyunyan1992@gmail.com>
2023-07-24 21:30:30 +0400

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+public class Main {
+    public static void main(String[] args) {
+        int price = 150;
+        int bonusMiles = 20;
+        int bonus = price / bonusMiles;
+        System.out.println("Итоговые мили" + bonus);
+
+
+    }
+}
